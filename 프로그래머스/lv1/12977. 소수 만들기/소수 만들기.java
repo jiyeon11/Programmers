a78e47977f07be5ed9df3234324c154f617001ff
이지연 <88700583+jiyeon11@users.noreply.github.com>
2023-08-18 09:07:02 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                for(int k = j+1; k<nums.length; k++){
+                    if(isPrime(nums[i]+nums[j]+nums[k])){
+                        answer++;
+                    } 
+                }
+            }
+        }
+
+        return answer;
+    }
+    //소수인지 판별하는 함수
+    boolean isPrime(int num){
+        for(int i = 2; i<=num; i++){
+            if(num != i && num%i == 0)
+                return false;
+        }
+        return true;
+    }
+}
